1bdba55bcd5800a1e2461b1cef8c0612de3932e7
Antoine <floresgomezanthony@gmail.com>
2021-08-19 15:58:24 -0400

    refactoring auth services/adding signup service

--- frontend/src/services/Auth.js
+++ frontend/src/services/Auth.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 // ToDo: add .env file for this routes
-const baseUrl = 'http://localhost:3001/auth/signin'
+const baseUrl = 'http://localhost:3001'
 
-const signIn = async credentials => {
-  const response = await axios.post(baseUrl, credentials)
+export const SignIn = async credentials => {
+  const response = await axios.post(`${baseUrl}/auth/signin`, credentials)
   return response.data
 }
 
-export default signIn
+export const SignUp = async user => {
+  const response = await axios.post(`${baseUrl}/api/users`, user)
+  return response.data
+}
