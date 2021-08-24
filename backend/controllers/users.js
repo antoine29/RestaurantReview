5fa0cccdfbab9156dd97e4023bed749f5806e427
antoine <anthonyfg9@gmail.com>
2021-08-24 01:22:17 -0400

    be: taking out data layer from controller layer

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,16 @@
-const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
-const User = require('../models/User')
+const { GetUser, GetUsers, CreateUser } = require('../dao/users')
 
 usersRouter.get('/', async (request, response) => {
-	const users = await User.find({}).populate('reviews', { user: 0 })
+	// const users = await User.find({}).populate('reviews', { user: 0 })
+	const users = await GetUsers()
 	response.json(users.map(u => u.toJSON()))
+})
+
+usersRouter.get('/:id', async (request, response) => {
+	const userId = request.params.id
+	const user = await GetUser(userId)
+	return user ? response.json(user) : response.sendStatus(402)
 })
 
 usersRouter.post('/', async (request, response) => {
@@ -12,19 +18,16 @@ usersRouter.post('/', async (request, response) => {
 		const body = request.body
 		if (body.password === undefined || body.password.length < 3)
 			return response.status(400).json({ error: '`password` field must be defined and must be greather than 3 chars. length.' })
-	
-		const saltRounds = 5
-		const passwordHash = await bcrypt.hash(body.password, saltRounds)
-	
-		const user = new User({
+
+		const newUser = {
 			username: body.username,
 			email: body.email,
 			name: body.name,
-			passwordHash,
-		})
-	
-		const savedUser = await user.save()
-		response.json(savedUser)
+			password: body.password
+		}
+
+		const savedUser = CreateUser(newUser)
+		return response.json(savedUser)
 	}
 	catch(error) {
 		return response.status(400).json({ error: error.message })
--- backend/dao/users.js
+++ backend/dao/users.js
@@ -1,10 +1,33 @@
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
+const GetUsers = async () => {
+	const users = await User.find({}).populate('reviews', { user: 0 })
+	return users
+}
+
 const GetUser = async (id) => {
-	const user = await User.findById(req.user.id)
+	const user = await User.findById(id)
     return user
 }
 
+const CreateUser = async (user) => {
+	const saltRounds = 5
+	const passwordHash = await bcrypt.hash(user.password, saltRounds)
+	
+	const newUser = new User({
+		username: user.username,
+		email: user.email,
+		name: user.name,
+		passwordHash,
+	})
+	
+	const savedUser = await newUser.save()
+	return savedUser
+}
+
 module.exports = {
-	GetUser
+	GetUsers,
+	GetUser,
+	CreateUser
 }
