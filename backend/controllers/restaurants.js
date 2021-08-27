edc54caa964f68f4bc640bb7a78ea64a1526b5f3
Antoine <floresgomezanthony@gmail.com>
2021-08-27 19:51:49 -0400

    be: adding post review response controller

--- backend/controllers/restaurants.js
+++ backend/controllers/restaurants.js
@@ -9,6 +9,7 @@ const {
     CreateRestaurantReview,
 	UpdateRestaurant
 } = require('../dao/restaurants')
+const { CreateReviewResponse, GetReview, UpdateReview } = require('../dao/reviews')
 
 restaurantsRouter.get('/', async (req, res) => {
 	try {
@@ -32,6 +33,7 @@ restaurantsRouter.get('/:id', async (req, res) => {
 	}
 })
 
+// ToDo: maybe not util, delete
 restaurantsRouter.get('/:id/reviews', async (req, res) => {
 	try {
 		const restaurantId = req.params.id
@@ -75,6 +77,35 @@ restaurantsRouter.post('/:id/reviews', async (req, res) => {
 		const updatedRestaurantRating = await CalculateRestaurantRating(restaurant._id)
 		if(updatedRestaurantRating) await UpdateRestaurant(restaurant._id, {rating: updatedRestaurantRating})
 		return res.status(200).json(savedReview)
+	}
+	catch(error){
+		return res.status(400).json({ error: error.message })
+	}
+})
+
+restaurantsRouter.post('/:restaurantId/reviews/:reviewId/response', async (req, res) => {    
+	try {
+        if (!req.token || !req.token.user) return res.status(401).json({ error: 'Missing token.'})
+		if (req.token.error) return res.status(401).json({ error: req.token.error})
+        
+        const existingUser = await GetUser(req.token.user.id)
+        if(!existingUser) return res.status(401).json({ error: 'User not found.'})
+        if(existingUser.role !== 'owner') {
+			console.log(0)
+			return res.status(401).json({ error: 'Invalid credentials.'})
+		}
+        
+        const existingRestaurant = await GetRestaurant(req.params.restaurantId)
+        if(!existingRestaurant) return res.status(401).json({ error: 'Restaurant not found.'})
+        if(JSON.stringify(existingRestaurant.owner) !== JSON.stringify(existingUser._id)) return res.status(401).json({ error: 'Invalid credentials.'})
+        
+        const existingReview = await GetReview(req.params.reviewId)
+		if(!existingReview) return res.status(401).json({ error: 'Review not found.'})
+		if(JSON.stringify(existingReview.restaurant) !== JSON.stringify(existingRestaurant._id)) return res.status(401).json({ error: 'Invalid paramters.'})
+
+		const savedReviewResponse = await CreateReviewResponse(existingUser._id, existingReview._id, req.body.response)
+		const updateReview = await UpdateReview(existingReview._id, { response: savedReviewResponse._id })
+		return res.status(200).json(savedReviewResponse)
 	}
 	catch(error){
 		return res.status(400).json({ error: error.message })
---
+++ backend/dao/reviews.js
@@ -0,0 +1,35 @@
+const ReviewResponse = require('../models/ReviewResponse')
+const Review = require('../models/Review')
+
+const GetReview = async (reviewId) => {	
+	const existingReview = await Review.findById(reviewId)
+	return existingReview
+}
+
+const CreateReviewResponse = async (userId, reviewId, response) => {	
+	const newReviewResponse = new ReviewResponse({
+		response,
+        review: reviewId,
+        user: userId
+	})
+	
+	const savedReviewResponse = await newReviewResponse.save()
+	return savedReviewResponse
+}
+
+const UpdateReview = async (reviewId, updatedReview) => {
+	const existingReview = await GetReview(reviewId)
+	if(updatedReview.comment) existingReview.comment = updatedReview.comment
+	if(updatedReview.stars) existingReview.stars = updatedReview.stars
+	if(updatedReview.restaurant) existingReview.restaurant = updatedReview.restaurant
+	if(updatedReview.user) existingReview.user = updatedReview.user
+	if(updatedReview.response) existingReview.response = updatedReview.response
+	await existingReview.save()
+	return existingReview
+}
+
+module.exports = {
+	GetReview,
+	CreateReviewResponse,
+	UpdateReview
+}
