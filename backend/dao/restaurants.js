3eb2a0b0d7e5c26c16ea43910e9a8241bda12d7d
antoine <anthonyfg9@gmail.com>
2021-08-26 21:34:08 -0400

    be: adding getRestaurantRating and update methods

--- backend/dao/restaurants.js
+++ backend/dao/restaurants.js
@@ -3,22 +3,11 @@ const Review = require('../models/Review')
 
 const GetRestaurants = async () => {
 	const restaurants = await Restaurant.find({})
-    const ratedRestaurants = await Promise.all(
-        restaurants.map(async (restaurant) => {
-            const rate = await GetRestaurantRate(restaurant)
-            restaurant._doc.rate = rate
-            return restaurant
-        })
-    )
-    
-	return ratedRestaurants
+	return restaurants
 }
 
-const GetRestaurant = async (id) => {
-	const restaurant = await Restaurant.findById(id)
-	if(!restaurant) return null
-    const rate = await GetRestaurantRate(restaurant)
-	restaurant._doc.rate = rate
+const GetRestaurant = async (restaurantId) => {
+	const restaurant = await Restaurant.findById(restaurantId)
 	return restaurant
 }
 
@@ -28,34 +17,18 @@ const CreateRestaurant = async (restaurant) => {
 	return savedRestaurant
 }
 
-const CreateRestaurantReview = async (review, restaurant, user) => {
+const CreateRestaurantReview = async (review, restaurantId, user) => {
+	
 	const newReview = new Review({
         ...review,
-        restaurant: restaurant.id,
+        restaurant: restaurantId,
         user: user.id
 	})
 	
 	const savedReview = await newReview.save()
+	// ToDo: do we need this populate?
 	const populatedReview = await Review.findById(savedReview.id).populate('user', {reviews: 0, role: 0})
 	return populatedReview
-}
-
-// ToDo: make this 'agregated' field part of the model?
-// i mean I think we can process it in one just query for each document
-const GetRestaurantRate = async (restaurant) => {
-    const starAverage =  await Review.aggregate([
-		{ $match: { restaurant: restaurant._id }},
-		{ $group: { _id: null, average: { $avg: '$stars' }}}
-	]).exec()
-
-	const rate = {
-		starAverage: starAverage.length > 0 ? starAverage[0].average : 0,
-		minRate: 0,
-		maxRate: 0,
-		totalReviews: 5
-	}
-
-    return rate
 }
 
 const GetRestaurantReviews = async (restaurantId) => {
@@ -63,10 +36,44 @@ const GetRestaurantReviews = async (restaurantId) => {
 	return reviews
 }
 
+const GetRestaurantRating = async (restaurantId) => {
+    const restaurantRating_result = await Review.aggregate([
+		{ $match: { restaurant: restaurantId }},
+		{ $group: {
+			_id: null,
+			averageStars: { $avg: '$stars' },
+			maxStar: { $max: "$stars" },
+			minStar: { $min: "$stars" },
+			totalStars: { $count: {} }
+		}}
+	]).exec()
+
+	const restaurantRating = restaurantRating_result.length > 0 ? restaurantRating_result[0] : null
+	console.log(`updated rating for restaurant ${restaurantId}:`, restaurantRating)
+	return restaurantRating
+}
+
+const UpdateRestaurant = async (restaurantId, newRestaurant) => {
+	const existingRestaurant = await GetRestaurant(restaurantId)
+	if (newRestaurant.name) existingRestaurant.name = newRestaurant.name 
+	if (newRestaurant.address) existingRestaurant.address = newRestaurant.address
+	if (newRestaurant.url) existingRestaurant.url = newRestaurant.url
+	if(newRestaurant.rating){
+		if (newRestaurant.rating.averageStars) existingRestaurant.rating.averageStars = newRestaurant.rating.averageStars
+		if (newRestaurant.rating.totalStars) existingRestaurant.rating.totalStars = newRestaurant.rating.totalStars
+		if (newRestaurant.rating.minStar) existingRestaurant.rating.minStar = newRestaurant.rating.minStar
+		if (newRestaurant.rating.maxStar) existingRestaurant.rating.maxStar = newRestaurant.rating.maxStar
+	}
+	await existingRestaurant.save()
+	return existingRestaurant
+}
+
 module.exports = {
 	GetRestaurants,
     GetRestaurant,
+	GetRestaurantRating,
+	GetRestaurantReviews,
     CreateRestaurant,
     CreateRestaurantReview,
-	GetRestaurantReviews
+	UpdateRestaurant
 }
