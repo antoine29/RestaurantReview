dc6e9422df7b0dd1c30a81cd1530b38d20a54fd6
antoine <anthonyfg9@gmail.com>
2021-08-27 23:19:14 -0400

    fe: deleting unused comments

--- frontend/src/components/Restaurants/Restaurants.js
+++ frontend/src/components/Restaurants/Restaurants.js
@@ -2,29 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { GetRestaurants } from '../../services/Restaurants';
 import { makeStyles } from '../UIComponents'
 
-import RestaurantCard from './RestaurantCard';
 // import Footer from './Footer';
+import { Button } from '../UIComponents'
+import RestaurantCard from './RestaurantCard';
 
-const useStyles = makeStyles((theme) => ({
-    mainGrid: {
-        marginTop: theme.spacing(3),
-    },
-}))
+// const useStyles = makeStyles((theme) => ({
+//     mainGrid: {
+//         marginTop: theme.spacing(3),
+//     },
+// }))
 
-const Restaurants = () => {
+const Restaurants = ({setToastState, setLoadingModal}) => {
     const [restaurants, setRestaurants] = useState([])
     useEffect(() => {
         const getRestaurantsCall = async () => {
+            setLoadingModal(true)
             const restaurants = await GetRestaurants()
             setRestaurants(restaurants)
+            setLoadingModal(false)
         }
 
         getRestaurantsCall()
     }, [])
 
     if (restaurants && restaurants.length > 0)
-        return (restaurants.map(restaurant => <RestaurantCard restaurant={restaurant} />))
-    else return (<div>NO RESTS</div>)
+        return (
+        <>
+        {
+            restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)
+        }
+         {/* <Button onClick={()=>{setToastState({severity: 'error', message: 'hii'})}}> click </Button> */}
+        </>
+        )
+    else return null
 }
 
 export default Restaurants
