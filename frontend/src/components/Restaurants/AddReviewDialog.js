f91d8942543687756a54c21e413a90a839b4a961
Antoine <floresgomezanthony@gmail.com>
2021-08-28 20:50:59 -0400

    fe: cleaning comments

--- frontend/src/components/Restaurants/AddReviewDialog.js
+++ frontend/src/components/Restaurants/AddReviewDialog.js
@@ -67,7 +67,6 @@ const AddReviewDialog = ({ openAddReview, setOpenAddReview, addReview }) => {
 				</Button>
 			</DialogActions>
 		</Dialog>
-
 	)
 }
 
--- frontend/src/components/Restaurants/Restaurants.js
+++ frontend/src/components/Restaurants/Restaurants.js
@@ -1,16 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { GetRestaurants } from '../../services/Restaurants';
-import { makeStyles } from '../UIComponents'
-
-// import Footer from './Footer';
-import { Button } from '../UIComponents'
-import RestaurantCard from './RestaurantCard';
-
-// const useStyles = makeStyles((theme) => ({
-//     mainGrid: {
-//         marginTop: theme.spacing(3),
-//     },
-// }))
+import React, { useState, useEffect } from 'react'
+import { GetRestaurants } from '../../services/Restaurants'
+import RestaurantCard from './RestaurantCard'
 
 const Restaurants = ({setToastState, setLoadingModal}) => {
     const [restaurants, setRestaurants] = useState([])
@@ -31,7 +21,6 @@ const Restaurants = ({setToastState, setLoadingModal}) => {
         {
             restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)
         }
-         {/* <Button onClick={()=>{setToastState({severity: 'error', message: 'hii'})}}> click </Button> */}
         </>
         )
     else return null
