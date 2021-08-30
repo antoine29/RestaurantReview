ea9653a9331be83b40f0d30b80cd92c9e7988682
Antoine <floresgomezanthony@gmail.com>
2021-08-29 22:28:59 -0400

    fe: enhancing restaurantReviewCard

--- frontend/src/components/Restaurants/RestaurantReviewCard.js
+++ frontend/src/components/Restaurants/RestaurantReviewCard.js
@@ -12,25 +12,34 @@ import {
   CardActions,
   CardHeader,
   Avatar,
-  red
+  red,
+  SendIcon,
+  CommentIcon,
+  Divider
 } from '../UIComponents'
 
+import TextAvatar from '../TextAvatar'
 import DeleteConfirmationDialog from '../DeleteConfirmationDIalog'
 import AddReviewResponseDialog from './AddReviewResponseDialog'
 import { CreateRestaurantReviewResponse, DeleteRestaurantReview } from '../../services/Restaurants'
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-})
+const useStyles = makeStyles((theme) => (
+  {
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+    card: {
+      marginBottom: theme.spacing(4),
+    },
+  }
+))
 
 const _addReviewResponse = async (restaurantId, reviewId, responseString, setLoadingModal, setToastState, setReloadRestaurant) => {
   setLoadingModal(true)
@@ -78,62 +87,58 @@ const RestaurantReviewCard = ({ review, ownerView, setLoadingModal, setToastStat
         setOpenAddResponse={setOpenAddResponse}
         sendResponse={sendResponse}
       />
-      <Grid item xs={12} md={6}>
-        <Card >
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe" className={classes.avatar}>
-                R
-              </Avatar>
-            }
-            title={review.user.username}
-            subheader={review.createdAt}
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {review.comment}
-            </Typography>
-          </CardContent>
+      <Card className={classes.card} >
+        <CardHeader
+          avatar={<TextAvatar text={review.user.name}/>}
+          title={review.user.username}
+          subheader={review.createdAt}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {review.comment}
+          </Typography>
+        </CardContent>
 
-          {!!review.response &&
-            <>
-              <CardHeader
-                avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    R
-                  </Avatar>
-                }
-                title={review.response.user.username}
-                subheader={review.createdAt}
-              />
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {review.response.response}
-                </Typography>
-              </CardContent>
-            </>
-          }
+        {!!review.response &&
+          <>
+            <Divider />
+            <CardHeader
+              avatar={ <TextAvatar text={review.response.user.name}/> }
+              title={review.response.user.username}
+              subheader={review.createdAt}
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {review.response.response}
+              </Typography>
+            </CardContent>
+          </>
+        }
 
-          {!review.response && ownerView &&
-            <CardActions disableSpacing>
-                <Button onClick={() => {
-                  setReviewIdToResponse(review.id)
-                  setOpenAddResponse(true)
-                }}>
-                  response
-                </Button>
-            </CardActions>
-              }
+        {!review.response && ownerView &&
+          <CardActions disableSpacing>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<CommentIcon />}
+              onClick={() => {
+                setReviewIdToResponse(review.id)
+                setOpenAddResponse(true)
+              }}
+            >
+              answer
+            </Button>
+          </CardActions>
+        }
 
-          {adminView &&
-              <CardActions disableSpacing>
-                <IconButton aria-label="Delete review" onClick={() => { setOpenDeleteDialog(true) }}>
-                  <DeleteIcon />
-                </IconButton>
-              </CardActions>
-            }
-        </Card>
-      </Grid>
+        {adminView &&
+          <CardActions disableSpacing>
+            <IconButton aria-label="Delete review" onClick={() => { setOpenDeleteDialog(true) }}>
+              <DeleteIcon />
+            </IconButton>
+          </CardActions>
+        }
+      </Card>
     </>
   )
 }
