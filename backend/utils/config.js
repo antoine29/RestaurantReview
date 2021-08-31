483ad26ca958cfc53d13cd3257329ba0fcecb16a
Antoine <floresgomezanthony@gmail.com>
2021-08-30 20:20:28 -0400

    adding dockerfile/dockercompose setup files

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -6,6 +6,9 @@ let MONGODB_URI = process.env.MONGODB_URI
 if (process.env.NODE_ENV === 'test')
 	MONGODB_URI = process.env.TEST_MONGODB_URI
 
+if (process.env.NODE_ENV === 'production')
+	MONGODB_URI = process.env.COMPOSE_MONGODB_URI
+
 module.exports = {
 	MONGODB_URI,
 	PORT
